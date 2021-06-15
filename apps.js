dab0cd0fa2430ef94f71cd2e82869eac5ee408f6
Eric Gómez <egomez_99@yahoo.com>
2021-06-14 19:56:08 -0500

    Clase virtual 16

---
+++ apps.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,'./Public')));
+
+app.listen(3000,() => console.log('Servidor corriendo en puerto 3000'));
+
+app.get('/',(req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./Views/home.html'));
+});
+
