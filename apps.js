8f9ca6e55501f09942f80abb75810a00d335f2b7
PauloEsau1 <pauloesau1696@hotmail.com>
2021-06-17 21:12:27 -0500

    Registrar

--- apps.js
+++ apps.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+const PORT = process.env.PORT || 80
 app.use(express.static(path.resolve(__dirname,'./Public')));
 
-app.listen(3000,() => console.log('Servidor corriendo en puerto 3000'));
+app.listen(PORT,() => console.log('Servidor corriendo en puerto 3000'));
 
 app.get('/',(req,res)=> {
     res.sendFile(path.resolve(__dirname,'./Views/home.html'));
